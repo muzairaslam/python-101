72b11efd8015b5c776e89fe26c69bde83a0fff2b
muzairaslam <m.uzair@statdevs.com>
2023-10-27 19:05:47 +0500

    Added class 5 content

---
+++ lectures/week-3/user-input-demo.py
@@ -0,0 +1,21 @@
+# Week-3 Input Lecture
+
+# Take user age as input 
+user_age = input("What is your age ? ")
+
+# Print the age
+print("User age is " + user_age)
+
+# Check the type of user_age
+print(type(user_age))
+
+# Perform some calculation on user_age
+double_user_age = int(user_age) * 2
+
+# Print the calculation
+print(double_user_age)
+
+# Check the type of double_user_age
+print(type(double_user_age))
+
+
