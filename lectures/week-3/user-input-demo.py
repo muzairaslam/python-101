feda8fd451e916b95fcedf5e8e4279324ab31d21
muzairaslam <m.uzair@statdevs.com>
2023-11-03 21:01:55 +0500

    added lecture seven notes

--- lectures/week-3/user-input-demo.py
+++ lectures/week-3/user-input-demo.py
@@ -1,26 +1,36 @@
 # Week-3 Input Lecture
 
 # Take user age as input 
-user_age = input("What is your age ? ")
+#user_age = input("What is your age ? ")
 
 # Print the age
-print("User age is " + user_age)
+#print("User age is " + user_age)
 
 # Check the type of user_age
-print(type(user_age))
+#print(type(user_age))
 
 # Perform some calculation on user_age
-double_user_age = int(user_age) * 2
+#double_user_age = int(user_age) * 2
 
 # Print the calculation
-print(double_user_age)
+#print(double_user_age)
 
 # Check the type of double_user_age
-print(type(double_user_age))
+#print(type(double_user_age))
 
 # convert integer data type to string
-str_double_age = str(double_user_age)
+#str_double_age = str(double_user_age)
 
 # now see the data type 
-print(type(str_double_age))
+#print(type(str_double_age))
+
+y = [1, 2, 3, 4, 5]
+
+# Copy the numbers 2 through 4 out of y and assign the slice to x.
+
+x = y[1:4]
+print(x)
+
+
+
 
